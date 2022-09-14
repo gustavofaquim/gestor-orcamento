7cddbecad2ba19e597092a614a0e5d3db73a0954
Gustavo Faquim <gustavofaquim408@gmail.com>
2022-09-14 15:43:15 -0300

    Criação e alteração de usuário

--- config/routes.php
+++ config/routes.php
@@ -3,15 +3,18 @@
 use \GestorOrcamento\Controllers\FormularioLogin;
 use \GestorOrcamento\Controllers\RealizaLogin;
 use \GestorOrcamento\Controllers\Deslogar;
-use \GestorOrcamento\Controllers\Usuario\FormularioCadastro;
+use \GestorOrcamento\Controllers\Usuario\FormularioCadastro as FormularioCadastroUsuario;
+use \GestorOrcamento\Controllers\Usuario\FormularioEdicao as FormularioEdicaoUsuario;
+use \GestorOrcamento\Controllers\Usuario\CadastroUsuario;
 
 
 $rotas = [
     '/login' => FormularioLogin::class,
     '/realiza-login' => RealizaLogin::class,
     '/deslogar' => Deslogar::class,
-    '/novo-usuario' => FormularioCadastro::class
-
+    '/novo-usuario' => FormularioCadastroUsuario::class,
+    '/cadastrar-usuario' => CadastroUsuario::class,
+    '/alterar-usuario' => FormularioEdicaoUsuario::class,
 ];
 
 return $rotas;
