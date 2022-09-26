084f5486355db57fd23481c4315f78ad4150530b
Gustavo Faquim <gustavofaquim408@gmail.com>
2022-09-26 09:14:53 -0300

    Atualizações diversar
    Inclusão do SCSS

--- view/usuario/formulario.php
+++ view/usuario/formulario.php
@@ -10,14 +10,31 @@
           <?= isset($usuario)? '': '<h4>Cadastre-se, é gratis!</h4>' ?>
 
             <form action="/cadastrar-usuario<?= isset($usuario) ? '?id= ' . $usuario->__get('id') : ''; ?>" class='form-login' method="post">
+              
               <div class="form-group">
-                <label for="email">Endereço de email ou Usuário</label>
+                <label for="email">Primeiro Nome</label>
+                <input type="text" class="form-control" id="primeiroNome" name='primeiroNome'  placeholder="Primeiro Nome" value="<?= isset($usuario) ? $usuario->__get('primeiroNome'): ''; ?>">
+              </div>
+
+              <div class="form-group">
+                <label for="email">Ultimo Nome</label>
+                <input type="text" class="form-control" id="ultimoNome" name='ultimoNome'  placeholder="Ultimo Nome" value="<?= isset($usuario) ? $usuario->__get('ultimoNome'): ''; ?>">
+              </div>
+            
+              <div class="form-group">
+                <label for="email">Endereço de E-mail</label>
                 <input type="text" class="form-control" id="email" name='email' aria-describedby="emailHelp" placeholder="E-mail ou Usuário" value="<?= isset($usuario) ? $usuario->__get('email'): ''; ?>">
                 <!--<small id="emailHelp" class="form-text text-muted">Nunca vamos compartilhar seus dados com ninguém.</small>-->
               </div>
+
               <div class="form-group">
                 <label for="senha">Senha</label>
                 <input type="password" class="form-control" id="senha" name='senha' placeholder="Senha" value="<?= isset($usuario) ? $usuario->__get('senha'): ''; ?>">
+              </div>
+
+              <div class="form-group">
+                <label for="senha">Confirmação de Senha</label>
+                <input type="password" class="form-control" id="senha2" name='senha2' placeholder=" Confirmação de Senha" value="<?= isset($usuario) ? $usuario->__get('senha'): ''; ?>">
               </div>
               
               <button type="submit" class="btn btn-primary btn-login">Salvar</button>
